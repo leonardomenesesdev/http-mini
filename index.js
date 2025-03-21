fb5f1130d05e91efb28df7d8a1bf3b11461bc48f
leonardomenesesdev <leonardomenesesc@edu.unifor.br>
2025-03-21 11:20:41 -0300

    comments

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-const express = require('express');  //MUITO importante explicar
+const express = require('express');
 const app = express();
-const cors = require('cors'); //MUITO importante explicar
+const cors = require('cors'); 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const port = 3000;
-app.use(cors()); //importante explicar
-app.use(express.json()); //importante explicar
+app.use(cors()); 
+app.use(express.json()); 
 
 
-
-
-
-//Explicar o metodo get do http
 app.get('/personagens', async (req, res) => {
   const personagens = await prisma.personagem.findMany();
   res.status(200).json(personagens);
 });
 
-//Explicar o metodo post do http
 app.post('/personagens', async (req, res) => {
     const {nome, casa} = req.body;
     try {
@@ -30,8 +25,6 @@ app.post('/personagens', async (req, res) => {
     }
 });
 
-//explicar put do http
-//explicar como o http reconhece o id
 app.put('/personagem/:id', async(req, res)=>{
     const {id} = req.params;
     const {nome, casa} = req.body;
@@ -46,7 +39,6 @@ app.put('/personagem/:id', async(req, res)=>{
     }
 })
 
-//explicar o patch e a diferença em relação ao put
 app.patch('/personagem/:id', async (req, res) => {
     const { id } = req.params;
     const { nome, casa } = req.body;
@@ -66,14 +58,14 @@ app.patch('/personagem/:id', async (req, res) => {
         res.status(500).json({ error: "Erro ao atualizar o personagem." });
     }
 });
-//explicar metodo delete no http
+
 app.delete('/personagem/:id', async (req, res) =>{
     const {id} = req.params;
     try {
         const personagemDeletado = await prisma.personagem.delete({
             where: {id_personagem: id}
         })
-        res.send(personagemDeletado);
+        res.json(personagemDeletado);
     } catch (error) {
         console.log(error);
     }
